27e1247eedf6bfadf5217d6e94d3d9bccfdfce54
fiserv25 <go4share.me@gmail.com>
2025-04-07 14:33:08 -0400

    push liquibase

---
+++ src/main/java/admin/user/User.java
@@ -0,0 +1,15 @@
+package admin.user;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+
+    // Getters and setters
+}
